01135918687a043dce9a9d4f8354951176a31874
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2020-06-30 00:25:04 -0400

    Update client.py

--- client.py
+++ client.py
@@ -7,11 +7,15 @@ import itertools
 import logging
 import socket
 import time
+import sys
+from random import choice
+from string import ascii_uppercase
 
 logger = logging.getLogger(__name__)
 
 # the buffer for receiving incoming messages
 BUFFER_SIZE = 4096
+
 
 def send_and_receive_one(sock, message, addr):
     "Sends a single datagram over the socket and waits for the response."
@@ -23,31 +27,34 @@ def send_and_receive_one(sock, message, addr):
     except socket.timeout:
         logger.warning("Message never received back from %s: (%s).", addr, message)
 
+
 def start(args):
     "Starts sending messages to the server."
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(1) # seconds
+    sock.settimeout(1)  # seconds
     addr = (args.host, args.port)
+    message = ''.join(choice(ascii_uppercase) for i in range(args.size))
     i = 1
     try:
         while i <= args.count:
-            message = "This is message #{}.".format(i)
             send_and_receive_one(sock, message, addr)
             i = i + 1
     finally:
         logger.info("Shutting down.")
         sock.close()
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--host', help='The host that the client should connect to.', default="127.0.0.1")
-    parser.add_argument('--port', help='The port that the client should connect to.', type=int, default=61592)
-    parser.add_argument('--verbose', '-v', help="Increases the logging verbosity level.", action='count')
-    parser.add_argument('--count', '-c', help='Number of udp packets to be sent', type=int, default=1)
+    parser = argparse.ArgumentParser(__doc__, formatter_class = argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--host', help = 'The host that the client should connect to.', default = "127.0.0.1")
+    parser.add_argument('--port', help = 'The port that the client should connect to.', type = int, default = 61592)
+    parser.add_argument('--verbose', '-v', help = "Increases the logging verbosity level.", action = 'count')
+    parser.add_argument('--count', '-c', help = 'Number of udp packets to be sent', type = int, default = 1)
+    parser.add_argument('--size', '-s', help = 'size of udp data to be sent in payload', type = int, default = 64)
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     args = parser.parse_args()
-    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
-                        format='%(asctime)s %(levelname)s %(message)s')
+    logging.basicConfig(level = logging.DEBUG if args.verbose else logging.INFO,
+                        format = '%(asctime)s %(levelname)s %(message)s')
     start(args)
