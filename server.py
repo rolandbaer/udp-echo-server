b02e1af140ffa0542931ef01caba38cb3138e8b1
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2020-06-30 00:24:06 -0400

    Update server.py

--- server.py
+++ server.py
@@ -6,11 +6,13 @@ import argparse
 import itertools
 import logging
 import socket
+import sys
 
 logger = logging.getLogger(__name__)
 
 # the buffer for receiving incoming messages
 BUFFER_SIZE = 4096
+
 
 def receive_next(sock):
     "Repeatedly tries receiving on the given socket until some data comes in."
@@ -22,6 +24,7 @@ def receive_next(sock):
             logger.debug("No data received yet: retrying.")
             pass
 
+
 def receive_and_send_one(sock):
     "Waits for a single datagram over the socket and echoes it back."
     input_data, addr = receive_next(sock)
@@ -30,10 +33,11 @@ def receive_and_send_one(sock):
     output_len = sock.sendto(input_data, addr)
     logger.info("Echoed message back to %s: %s (%s bytes).", addr, message, output_len)
 
+
 def start(args):
     "Runs the server."
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(5) # seconds
+    sock.settimeout(5)  # seconds
     sock.bind((args.host, args.port))
     logger.info("Listening on %s:%s.", args.host, args.port)
     try:
@@ -43,15 +47,16 @@ def start(args):
         logger.info("Shutting down.")
         sock.close()
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--host', help='The host that the server socket should bind to.', default="0.0.0.0")
-    parser.add_argument('--port', help='The port that the server socket should bind to.', type=int, default=61592)
-    parser.add_argument('--verbose', '-v', help="Increases the logging verbosity level.", action='count')
+    parser = argparse.ArgumentParser(__doc__, formatter_class = argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--host', help = 'The host that the server socket should bind to.', default = "0.0.0.0")
+    parser.add_argument('--port', help = 'The port that the server socket should bind to.', type = int, default = 61592)
+    parser.add_argument('--verbose', '-v', help = "Increases the logging verbosity level.", action = 'count')
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
     args = parser.parse_args()
-    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
-                        format='%(asctime)s %(levelname)s %(message)s')
+    logging.basicConfig(level = logging.DEBUG if args.verbose else logging.INFO,
+                        format = '%(asctime)s %(levelname)s %(message)s')
     start(args)
